850f1a799a349713435ef559c7eb728372aeeb30
AN-MBANK <mastershining000@example.com>
2026-02-16 15:39:02 +0700

    fix cors preflight

--- MBA-Gucci/server/src/index.js
+++ MBA-Gucci/server/src/index.js
@@ -9,11 +9,15 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(cors({
   origin: "https://project-final-brown.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
+app.options("*", cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
