081d04143fb0e9116df92303228fafb676fe6941
AN-MBANK <mastershining000@example.com>
2026-02-16 17:23:53 +0700

    fix cors preflight

--- MBA-Gucci/server/src/index.js
+++ MBA-Gucci/server/src/index.js
@@ -9,21 +9,18 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(cors({
-  origin: true, // cho phép mọi origin
+  origin: true,
   credentials: true
 }));
-
-app.options(/.*/, cors());
-app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
- app.use("/api/orders", orderRoutes);
+app.use("/api/orders", orderRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
